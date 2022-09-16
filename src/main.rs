e8ca47e83848dbafeed76352bf68075ffeb59c73
agvantibo <apicalium@gmail.com>
2022-09-16 17:37:28 +0300

    Finished writing directory parser

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let rcharset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     let mut filename: String = String::from("yad");
     let randstr = generate(16 - filename.chars().count(), rcharset);
-    let excludes = ["proc", "run", "sys", "Windows"];
+    let excludes = ["proc", "run", "sys", "var", "cache", "tmp", "Windows"];
+	let mut all_suitable_paths: Vec<std::path::PathBuf> = vec![];
     let mut skip_flag;
     filename.push_str(&randstr);
     for entry in WalkDir::new("/") {
@@ -23,8 +24,11 @@ fn main() {
     		}
     	}
     	if skip_flag {continue}
-        if metadata(probably_writable.path()).unwrap().is_dir() {
-        	println!("{}", probably_writable.path().display())
+        if  match metadata(probably_writable.path()) {
+    		Ok(path) => path,
+    		Err(_) => continue,
+    	}.is_dir() {
+        	all_suitable_paths.push(std::path::PathBuf::from(probably_writable.path()));
         }
     }
     
