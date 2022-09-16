3a8960f93e1f29e20189c37b8e6060928bc82470
Savchenko Dmitriy <apicalium@gmail.com>
2022-09-16 16:14:12 +0300

    Fixed .contains compiler warning

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use random_string::generate;
-use std::io;
-use std::fs;
+//use std::io;
+//use std::fs;
 use walkdir::WalkDir;
 use std::fs::metadata;
 
@@ -8,11 +8,23 @@ fn main() {
     let rcharset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     let mut filename: String = String::from("yad");
     let randstr = generate(16 - filename.chars().count(), rcharset);
+    let excludes = ["proc", "run", "sys", "Windows"];
+    let mut skip_flag;
     filename.push_str(&randstr);
-    for entry in WalkDir::new(".") {
-    	let probably_writable = entry.unwrap_or_else(|error| {continue}).path();
-        if metadata(probably_writable).unwrap().is_dir() {
-        	
+    for entry in WalkDir::new("/") {
+    	skip_flag = false;
+    	let probably_writable = match entry {
+    		Ok(path) => path,
+    		Err(_) => continue,
+    	};
+    	for badname in excludes {
+    		if probably_writable.path().to_str().expect("Something has gone terribly wrong").to_string().contains(&badname){
+    			skip_flag = true;
+    		}
+    	}
+    	if skip_flag {continue}
+        if metadata(probably_writable.path()).unwrap().is_dir() {
+        	println!("{}", probably_writable.path().display())
         }
     }
     
