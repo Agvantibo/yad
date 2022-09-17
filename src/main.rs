4a538a59d6fca07d5db91b3ce0458e88b2bf11c2
agvantibo <apicalium@gmail.com>
2022-09-17 13:18:33 +0300

    Non-replicating self-copying yad done. \nCurability ensured as of now

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use random_string::generate;
-//use std::io;
-//use std::fs;
-use walkdir::WalkDir;
+extern crate random_string;
+extern crate dirs;
+extern crate walkdir;
 use std::fs::metadata;
+use std::io::Write;
+use std::path::Path;
+use std::fs;
+use std::env;
 
 fn main() {
     let rcharset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	let curepath = env::var("YAD_CURE").unwrap_or(".curefile".to_owned());
+	let destructive = env::var("YAD_FILES").is_ok();
+	let args: Vec<String> = env::args().collect();
     let mut filename: String = String::from("yad");
-    let randstr = generate(16 - filename.chars().count(), rcharset);
+    let randstr = random_string::generate(16 - filename.chars().count(), rcharset);
     let excludes = ["proc", "run", "sys", "var", "cache", "tmp", "Windows"];
 	let mut all_suitable_paths: Vec<std::path::PathBuf> = vec![];
     let mut skip_flag;
     filename.push_str(&randstr);
-    for entry in WalkDir::new("/") {
+	let mut curefile = match fs::File::create(curepath) {
+		Ok(file) => file,
+		Err(_) => panic!("Cure file creation in the specified location failed. Refusing to continue, permanent damage possible."),
+	};
+    for entry in walkdir::WalkDir::new("/") {
     	skip_flag = false;
     	let probably_writable = match entry {
     		Ok(path) => path,
@@ -25,11 +35,27 @@ fn main() {
     	}
     	if skip_flag {continue}
         if  match metadata(probably_writable.path()) {
-    		Ok(path) => path,
+    		Ok(md) => md,
     		Err(_) => continue,
     	}.is_dir() {
         	all_suitable_paths.push(std::path::PathBuf::from(probably_writable.path()));
         }
     }
+	for yadir_path in all_suitable_paths.iter() {
+		let mut yad_pathstr = String::from(yadir_path.to_str().unwrap());
+		yad_pathstr.push_str(&filename.to_owned());
+		let yad_path = Path::new(&yad_pathstr);
+		if destructive {
+			match fs::copy(Path::new(&args[0]), yad_path) {
+				Ok(_) => break,
+				Err(_) => continue,
+			};
+		};
+		match curefile.write_all(format!("\n{}", yad_pathstr).as_bytes()) {
+			Ok(_) => println!("{}", yad_pathstr),
+			Err(_) => panic!("Cure file write failed. Refusing to continue, permanent damage possible.")
+		};
+	}
+
     
 }
